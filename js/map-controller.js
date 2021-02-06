ae10a407995c8837e7939080bbb4fd0c728f21e8
noyhason3 <noyhason3@gmail.com>
2021-02-06 13:36:26 +0200

    6

--- js/map-controller.js
+++ js/map-controller.js
@@ -1,3 +1,13 @@
 function mapReady() {
     console.log('Map is ready');
 }
+
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
