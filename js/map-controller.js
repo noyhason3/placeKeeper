d55800d06cbb357a620f932139613c6a00205522
noyhason3 <noyhason3@gmail.com>
2021-02-06 14:59:21 +0200

    8

--- js/map-controller.js
+++ js/map-controller.js
@@ -1,12 +1,19 @@
 let map;
 
-function initMap() {
+function initMap(lat= 29.55805, lng= 34.94821 ) {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat, lng},
+    zoom: 16,
   });
+  console.log('Map is ready');
 }
 
-function mapReady() {
-  console.log('Map is ready');
+function setCenter() {
+  navigator.geolocation.getCurrentPosition(() => {
+    const pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+    initMap(pos.lat,pos.lng)
+  });
 }
